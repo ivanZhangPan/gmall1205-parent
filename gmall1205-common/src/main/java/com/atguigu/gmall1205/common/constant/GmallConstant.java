9912779238fdf099a05f4a7cf428ebd30d9c51e2
136101157@qq.com <zhangpan8015>
2020-07-27 14:53:33 +0800

    first commit

---
+++ gmall1205-common/src/main/java/com/atguigu/gmall1205/common/constant/GmallConstant.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall1205.common.constant;
+
+public class GmallConstant {
+
+
+    public static final String KAFKA_TOPIC_STARTUP="GMALL_STARTUP";
+    public static final String KAFKA_TOPIC_EVENT="GMALL_EVENT";
+    public static final String KAFKA_TOPIC_ORDER="GMALL_ORDER";
+    public static final String  KAFKA_TOPIC_ORDER_DETAIL="GMALL_ORDER_DETAIL";
+    public static final String  KAFKA_TOPIC_USER="GMALL_USER";
+
+    public static final String ES_INDEX_DAU="gmall1205_dau";
+    public static final String ES_INDEX_ORDER="gmall1205_order";
+    public static final String ES_INDEX_SALE="gmall1205_sale_detail";
+    public static final String  ES_INDEX_ALERT="gmall_coupon_alert0715";
+}
